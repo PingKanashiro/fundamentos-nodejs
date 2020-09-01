ad1076f06f4aa9d58e7fe83ce29745811a1475f7
Fernando Kanashiro <fernando_skanashiro@hotmail.com>
2020-09-01 14:54:10 -0300

    Fundamentos Node, Transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const totalIncome = income.reduce(
+      (total, { value }: { value: number }) => total + value,
+      0,
+    );
+
+    const outcome = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const totalOutcome = outcome.reduce(
+      (total, { value }: { value: number }) => total + value,
+      0,
+    );
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
